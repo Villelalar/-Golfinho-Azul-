49cc0bbfdff8922438432fe30465eb97ed70eb45
mochilicius <giovanna.poggi@outlook.com>
2025-06-03 15:51:29 -0300

    dropdown fix

--- app.py
+++ app.py
@@ -729,4 +729,4 @@ def test_db():
             connection.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
